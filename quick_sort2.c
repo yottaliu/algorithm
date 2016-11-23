37aecc2415c46d59bd7a6b38a104e9474b995970
Yotta Liu <yotta.liu@gmail.com>
2016-11-23 11:59:40 +0800

    添加新的快速排序算法，可以处理各种极端情况，如所有值全相等，已经经过预排序等情况

---
+++ quick_sort2.c
@@ -0,0 +1,68 @@
+typedef int ElementType;
+
+void swap(ElementType * one, ElementType * two)
+{
+    ElementType temp;
+
+    temp = *one;
+    *one = *two;
+    *two = temp;
+}
+
+ElementType Median3(ElementType A[], int left, int right)
+{
+    int center;
+
+    center = (left + right) / 2;
+    if (A[left] > A[center]) {
+        swap(&A[left], &A[center]);
+    }
+    if (A[left] > A[right]) {
+        swap(&A[left], &A[right]);
+    }
+    if (A[center] > A[right]) {
+        swap(&A[center], &A[right]);
+    }
+    swap(&A[center], &A[right]);
+
+    return A[right];
+}
+
+#define cutoff (3)
+
+void Qsort(ElementType A[], int left, int right)
+{
+    int i, j;
+    ElementType pivot;
+
+    if (left + cutoff <= right) {
+        pivot = Median3(A, left, right);
+        i = left;
+        j = right - 1;
+        while (1)
+        {
+            while (A[++i] < pivot) {
+                continue;
+            }
+            while (A[--j] > pivot) {
+                continue;
+            }
+            if (i < j) {
+                swap(&A[i], &A[j]);
+            } else {
+                break;
+            }
+        }
+        swap(&A[i], &A[right-1]);
+
+        Qsort(A, left, i-1);
+        Qsort(A, i+1, right);
+    } else {
+        InsertionSort(A+left, right-left+1);
+    }
+}
+
+void QuickSort(ElementType A[], int n)
+{
+    Qsort(A, 0, n-1);
+}
