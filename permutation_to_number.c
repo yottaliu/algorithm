9f2a406a517f3c59af7e7323a7cfd288b62008b2
Yotta Liu <yotta.liu@gmail.com>
2017-03-18 13:35:53 +0800

    添加一个算法，由一个用Lehmer code编码的排列，求出此排列的编号。

---
+++ permutation_to_number.c
@@ -0,0 +1,55 @@
+/*
+ * 标题： 排列序数
+ *
+ * X星系的某次考古活动发现了史前智能痕迹。
+ * 这是一些用来计数的符号，经过分析它的计数规律如下：
+ * （为了表示方便，我们把这些奇怪的符号用a~q代替）
+ *
+ * abcdefghijklmnopq 表示0
+ * abcdefghijklmnoqp 表示1
+ * abcdefghijklmnpoq 表示2
+ * abcdefghijklmnpqo 表示3
+ * abcdefghijklmnqop 表示4
+ * abcdefghijklmnqpo 表示5
+ * abcdefghijklmonpq 表示6
+ * abcdefghijklmonqp 表示7
+ * .....
+ *
+ * 在一处石头上刻的符号是：
+ * bckfqlajhemgiodnp
+ *
+ * 请你计算出它表示的数字是多少？
+ *
+ * 请提交该整数，不要填写任何多余的内容，比如说明或注释。
+ *
+ * 问题中的编码其实就是Lehmer code
+ * 这是求Lehmer code的逆过程，思路是先由给出的排列求出这个排列的Lehmer code，再由Lehmer code倒推出这个排列的编号。
+ */
+
+#include <stdio.h>
+#define N 17
+int main(void)
+{
+    int i, j, pos[N], code[N];
+    char perm[N+1];
+    long long int fac[N], num = 0;
+    scanf("%s", perm);
+    fac[0] = 1;
+    for (i = 1; i < N; ++i) {
+        fac[i] = (long long int)i * fac[i-1];
+    }
+    for (i = 0; i < N; ++i) {
+        pos[i] = i;
+    }
+    for (i = 0; i < N; ++i) {
+        code[i] = pos[perm[i]-'a'];
+        for (j = perm[i]-'a'+1; j < N; ++j) {
+            pos[j]--;
+        }
+    }
+    for (i = 1; i < N; ++i) {
+        num += code[N-1-i] * fac[i];
+    }
+    printf("%lld\n", num);
+    return 0;
+}
