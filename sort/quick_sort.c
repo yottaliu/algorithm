2580fa2156f19f766b95cb0da719e1ac75d707b4
Yotta Liu <yotta.liu@gmail.com>
2017-05-01 15:03:10 +0800

    把quick_sort中的swap独立出来了

--- sort/quick_sort.c
+++ sort/quick_sort.c
@@ -1,42 +1,46 @@
 #include "quick_sort.h"
 
+static void swap(Item * one, Item * two)
+{
+    Item tmp = *one;
+    *one = *two;
+    *two = tmp;
+}
+
 static int partition(Item *arr, int left, int right)
 {
-    int i, j;
-    Item tmp, t;
+    int i = left;
+    int j = right;
+    Item pivot = arr[left];
 
-    i = left;
-    j = right;
-    tmp = arr[left];
     while (i < j) {
-        while (i < j && arr[j] >= tmp) {
+        while (i < j && arr[j] >= pivot) {
             --j;
         }
-        while (i < j && arr[i] <= tmp) {
+        while (i < j && arr[i] <= pivot) {
             ++i;
         }
         if (i < j) {
-            t = arr[i];
-            arr[i] = arr[j];
-            arr[j] = t;
+            swap(&arr[i], &arr[j]);
         }
     }
-    arr[left] = arr[i];
-    arr[i] = tmp;
+    swap(&arr[left], &arr[i]);
 
     return i;
 }
 
 static void qsort(Item *arr, int left, int right)
 {
-    int pivot;
-    if (left > right) {
+    int pivot_index;
+
+    if (left >= right) {
         return;
     }
 
-    pivot = partition(arr, left, right);
-    qsort(arr, left, pivot-1);
-    qsort(arr, pivot+1, right);
+    pivot_index = partition(arr, left, right);
+
+    qsort(arr, left, pivot_index-1);
+    qsort(arr, pivot_index+1, right);
 }
 
 int quick_sort(Item *arr, int n)
